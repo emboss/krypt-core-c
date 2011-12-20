5ec314aaa7f60bdda4277331bdfa20a04d6785e2
Martin Boßlet <Martin.Bosslet@googlemail.com>
2011-12-20 03:24:37 +0100

    Fixed krypt_instream implementation for StringIO

--- ext/krypt/core/krypt_io_generic.c
+++ ext/krypt/core/krypt_io_generic.c
@@ -30,13 +30,13 @@ krypt_instream_new_io_generic(VALUE io)
     VALUE buf;
 
     in = krypt_instream_new(&interface_io_generic);
-    in->ptr = (void *)&io;
+    in->ptr = (void *)io;
     cbuf = (unsigned char *)xmalloc(KRYPT_IO_BUF_SIZE);
     in->buf = cbuf;
-    buf = rb_str_new((const char *)cbuf, KRYPT_IO_BUF_SIZE);
+    buf = rb_str_new2("");
     /* exclude it from GC */
     rb_gc_register_address(&buf);
-    in->util = (void *)&buf;
+    in->util = (void *)buf;
     in->buf_len = KRYPT_IO_BUF_SIZE;
     return in;
 }
@@ -48,12 +48,17 @@ int_io_rb_read(krypt_instream *in, VALUE buf, VALUE len)
 
     if (buf == Qnil) return 0;
 
-    io = *((VALUE *)in->ptr);
+    io = (VALUE)in->ptr;
     read = rb_funcall(io, ID_READ, 2, len, buf);
-    if (read == Qnil)
+    if (read == Qnil) {
 	return -1;
-    else
-	return RSTRING_LENINT(read);
+    }
+    else {
+	int r;
+	r = RSTRING_LENINT(read);
+	memcpy(in->buf, RSTRING_PTR(read), r);
+	return r;
+    }
 }
 
 static int
@@ -61,7 +66,7 @@ int_io_read(krypt_instream *in, int len)
 {
     VALUE buf, vlen;
 
-    buf = *((VALUE *)in->util);
+    buf = (VALUE)in->util;
     vlen = INT2NUM(len > in->buf_len ? in->buf_len : len);
     /* no need to update in->num_read */
     return int_io_rb_read(in, buf, vlen);
@@ -70,14 +75,14 @@ int_io_read(krypt_instream *in, int len)
 static int
 int_io_free(krypt_instream *in)
 {
-    VALUE *buf;
+    VALUE buf;
 
     if (!in)
 	return 0;
 
-    buf = (VALUE *)in->util;
+    buf = (VALUE)in->util;
     /* give it free for GC, also frees the byte array */
-    rb_gc_unregister_address(buf); 
+    rb_gc_unregister_address(&buf); 
     return 1; 
 }
 
--- test/test.rb
+++ test/test.rb
@@ -1,6 +1,7 @@
 require 'krypt-core'
 require_relative 'resources'
 require 'stringio'
+require 'pp'
 
 io = Resources.certificate_io
 parser = Krypt::Asn1::Parser.new
@@ -12,13 +13,15 @@ puts header.header_length
 puts header.length
 puts
 
-#io = StringIO.new(Resources.certificate)
-#parser = Krypt::Asn1::Parser.new
-#header = parser.next(io)
-#puts header.tag
-#puts header.tag_class
-#puts header.header_length
-#puts header.length
+io = StringIO.new(Resources.certificate)
+
+2.times do 
+  header = parser.next(io)
+  puts header.tag
+  puts header.tag_class
+  puts header.header_length
+  puts header.length
+end
 
 
 
