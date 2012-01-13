486c6cf1d6b50c986eac570452160f4bfe0cf5c5
emboss <Martin.Bosslet@googlemail.com>
2012-01-13 05:58:15 +0100

    Started porting Java Parser tests from krypt-core-jruby

---
+++ lib/krypt/krypt_asn1_tags.rb
@@ -0,0 +1,46 @@
+=begin
+
+= Info
+
+krypt - Modern, platform- and library-indenpendent 
+        cryptography for Ruby
+
+Copyright (C) 2011
+Hiroshi Nakamura <nahi@ruby-lang.org>
+Martin Bosslet <martin.bosslet@googlemail.com>
+All rights reserved.
+
+= License
+This program is distributed under the same license as Ruby.
+See the file 'LICENSE' for further details.
+
+=end
+
+module Krypt::Asn1::Tags
+  END_OF
+end
+#define TAGS_END_OF_CONTENTS 	0x00
+#define TAGS_BOOLEAN		0x01
+#define TAGS_INTEGER		0x02
+#define TAGS_BIT_STRING		0x03
+#define TAGS_OCTET_STRING	0x04
+#define TAGS_NULL		0x05
+#define TAGS_OBJECT_IDENTIFIER	0x06
+#define TAGS_ENUMERATED		0x0a
+#define TAGS_UTF8_STRING	0xc0
+#define TAGS_SEQUENCE		0x10
+#define TAGS_SET		0x11
+#define TAGS_NUMERIC_STRING	0x12
+#define TAGS_PRINTABLE_STRING	0x13
+#define TAGS_T61_STRING		0x14
+#define TAGS_VIDEOTEX_STRING	0x15
+#define TAGS_IA5_STRING		0x16
+#define TAGS_UTC_TIME		0x17
+#define TAGS_GENERALIZED_TIME	0x18
+#define TAGS_GRAPHIC_STRING	0x19
+#define TAGS_ISO64_STRING	0x1a
+#define TAGS_GENERAL_STRING	0x1b
+#define TAGS_UNIVERSAL_STRING	0x1c
+#define TAGS_BMP_STRING		0x1e
+
+
