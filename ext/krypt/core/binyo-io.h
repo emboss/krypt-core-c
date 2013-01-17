3a658a1bf72361f0cc0985b19b75f569945ad956
Martin Boßlet <Martin.Bosslet@gmail.com>
2013-01-18 00:24:22 +0100

    MIT license

--- ext/krypt/core/binyo-io.h
+++ ext/krypt/core/binyo-io.h
@@ -1,12 +1,30 @@
 /*
-* binyo - Fast binary IO for Ruby
-*
-* Copyright (C) 2011-2013
-* Martin Bosslet <martin.bosslet@gmail.com>
-* All rights reserved.
-*
-* See the file 'LICENSE' for further details about licensing.
-*/
+ * binyo - Fast binary IO for Ruby
+ *
+ * Copyright (c) 2012-2013
+ * Hiroshi Nakamura <nahi@ruby-lang.org>
+ * Martin Bosslet <martin.bosslet@gmail.com>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #if !defined(_BINYO_IO_H_)
 #define _BINYO_IO_H_
